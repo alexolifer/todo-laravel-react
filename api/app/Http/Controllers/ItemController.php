2dd8a7d159aa20d19baa1478cbb4136d48d3907d
Alexandre Ferreira <alexandreoliferr@gmail.com>
2021-09-30 01:36:59 +0100

    Created the Laravel and React apps

---
+++ api/app/Http/Controllers/ItemController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::orderBy('created_at', 'DESC')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Item
+     */
+    public function store(Request $request): Item
+    {
+        $newItem = new Item();
+        $newItem->name = $request->item["name"];
+        $newItem->save();
+
+        return $newItem;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, int $id): \Illuminate\Http\Response|string
+    {
+        $existingItem = Item::find($id);
+
+        if (!$existingItem) {
+            return "O item indicado não foi encontrado.";
+        }
+        $existingItem->completed = (bool)$request->item['completed'];
+        $existingItem->completed_at = $request->item['completed'] ? Carbon::now() : null;
+        $existingItem->save();
+        return $existingItem;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return string
+     */
+    public function destroy($id)
+    {
+        $existingItem = Item::find($id);
+
+        if (!$existingItem) {
+            return "O item indicado não foi encontrado.";
+        }
+
+        $existingItem->delete();
+        return "Item excluido com sucesso.";
+
+    }
+}
